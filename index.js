121bc59616cfc368f8c1fe31f1b46af0b80eac17
wrenchorizo <goldenboyf6@gmail.com>
2026-03-16 20:17:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-{}
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        dataPath: '/data/session' // 🔥 aquí se guarda todo
+    })
+});
+
+client.on('qr', (qr) => {
+    console.log('Escanea este QR:');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('🤖 MikuBot está listo!');
+});
+
+client.on('message', message => {
+    if (message.body === 'hola') {
+        message.reply('🎤 ¡Hola! Soy Miku~ ✨');
+    }
+});
+
+client.initialize();
