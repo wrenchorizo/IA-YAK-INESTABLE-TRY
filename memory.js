2413ea823ec832c30e7ce50da2093a133e72c3b5
wrenchorizo <goldenboyf6@gmail.com>
2026-03-16 20:33:42 -0600

    Create memory.js

---
+++ memory.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const path = '/data/memory.json';
+
+function cargarMemoria() {
+    if (!fs.existsSync(path)) {
+        fs.writeFileSync(path, JSON.stringify({}));
+    }
+    return JSON.parse(fs.readFileSync(path));
+}
+
+function guardarMemoria(data) {
+    fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+function getUser(userId) {
+    const data = cargarMemoria();
+
+    if (!data[userId]) {
+        data[userId] = {
+            mensajes: 0,
+            afinidad: 0,
+            recuerdos: []
+        };
+    }
+
+    return data[userId];
+}
+
+function updateUser(userId, userData) {
+    const data = cargarMemoria();
+    data[userId] = userData;
+    guardarMemoria(data);
+}
+
+module.exports = { getUser, updateUser };
